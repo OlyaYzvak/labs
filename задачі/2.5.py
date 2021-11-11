736a2ce781cda8a836b3ace44f088acfb83f1b89
OlyaYuzvak <olyayuzvak@icloud.com>
2021-11-11 20:03:49 +0200

    задачі 2

--- задачі/2.5.py
+++ задачі/2.5.py
@@ -1,7 +1,9 @@
 a = int(input("Введіть число 1: "))
-b = 9184, int(input("Введіть число 2: "))
-s = 0
-for i in range(54, 9184):
-    if i % 5  == 0:
-        s = s + i
+b = int(input("Введіть число 2: "))
+
+for i in range(a, b):
+    if i % 5 == 0:
         print(i)
+
+
+
